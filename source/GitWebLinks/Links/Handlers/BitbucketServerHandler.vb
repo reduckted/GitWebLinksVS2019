0ac0fe7574a97640404c362ce012d88958a1bdb4
reduckted <reduckted@outlook.com>
2017-07-15 15:00:09 +1000

    Added a handler for Bitbucket Cloud.

---
+++ source/GitWebLinks/Links/Handlers/BitbucketServerHandler.vb
@@ -0,0 +1,87 @@
+﻿Imports LibGit2Sharp
+Imports System.ComponentModel.Composition
+Imports System.Text.RegularExpressions
+
+<Export(GetType(ILinkHandler))>
+Public Class BitbucketServerHandler
+    Inherits LinkHandlerBase
+
+
+    Private Shared ReadOnly ProjectPattern As New Regex("(?<project>[^\/]+)\/(?<repo>[^\/]+)$")
+
+
+    Private ReadOnly cgOptions As IOptions
+
+
+    <ImportingConstructor()>
+    Public Sub New(options As IOptions)
+        cgOptions = options
+    End Sub
+
+
+    Public Overrides ReadOnly Property Name As String
+        Get
+            Return "Bitbucket"
+        End Get
+    End Property
+
+
+    Protected Overrides Function GetServerUrls() As IEnumerable(Of ServerUrl)
+        Return cgOptions.BitbucketServerUrls
+    End Function
+
+
+    Protected Overrides Function GetBranchName(branch As Branch) As String
+        Return branch.CanonicalName
+    End Function
+
+
+    Protected Overrides Function CreateUrl(
+            baseUrl As String,
+            repositoryPath As String,
+            branch As String,
+            relativePathToFile As String
+        ) As String
+
+        Dim url As String
+        Dim projectMatch As Match
+        Dim project As String
+        Dim repo As String
+
+
+        projectMatch = ProjectPattern.Match(repositoryPath)
+
+        If Not projectMatch.Success Then
+            Throw New ApplicationException("Could not find the project and repository names in the remote URL.")
+        End If
+
+        project = projectMatch.Groups("project").Value
+        repo = projectMatch.Groups("repo").Value
+
+        url = String.Join("/", {baseUrl, "projects", project, "repos", repo, "browse", relativePathToFile})
+
+        ' The branch name is specified via a query parameter.
+        url &= $"?at={Uri.EscapeDataString(branch)}"
+
+        Return url
+    End Function
+
+
+    Protected Overrides Function GetSelectionHash(
+            filePath As String,
+            selection As LineSelection
+        ) As String
+
+        Dim hash As String
+
+
+        hash = $"#{selection.StartLineNumber}"
+
+        If selection.StartLineNumber <> selection.EndLineNumber Then
+            hash &= $"-{selection.EndLineNumber}"
+        End If
+
+        Return hash
+    End Function
+
+End Class
---
+++ tests/GitWebLinks.Tests/Links/Handlers/BitbucketServerHandlerTests.vb
@@ -0,0 +1,227 @@
+﻿Imports LibGit2Sharp
+Imports System.IO
+
+
+Public Class BitbucketServerHandlerTests
+
+    Public Class NameProperty
+
+        <Fact()>
+        Public Sub ReturnsBitbucket()
+            Assert.Equal("Bitbucket", CreateHandler().Name)
+        End Sub
+
+    End Class
+
+
+    Public Class IsMatchMethod
+
+        <Theory()>
+        <MemberData(NameOf(GetRemotes), MemberType:=GetType(BitbucketServerHandlerTests))>
+        Public Sub MatchesServerUrlsFromSettings(remote As String)
+            Dim handler As BitbucketServerHandler
+
+
+            handler = CreateHandler({New ServerUrl("https://local-bitbucket:7990/context", "git@local-bitbucket:7999")})
+
+            Assert.True(handler.IsMatch(remote))
+        End Sub
+
+
+        <Theory()>
+        <MemberData(NameOf(GetRemotes), MemberType:=GetType(BitbucketServerHandlerTests))>
+        Public Sub DoesNotMatchServerUrlsNotInSettings(remote As String)
+            Dim handler As BitbucketServerHandler
+
+
+            handler = CreateHandler()
+
+            Assert.False(handler.IsMatch("https://codeplex.com/foo/bar.git"))
+        End Sub
+
+    End Class
+
+
+    Public Class MakeUrlMethod
+
+        <Theory()>
+        <MemberData(NameOf(GetRemotes), MemberType:=GetType(BitbucketServerHandlerTests))>
+        Public Sub CreatesCorrectLinkFromRemoteUrl(remote As String)
+            Using dir As New TempDirectory
+                Dim handler As BitbucketServerHandler
+                Dim info As GitInfo
+                Dim fileName As String
+
+
+                info = New GitInfo(dir.FullPath, remote)
+                fileName = Path.Combine(dir.FullPath, "lib\server\main.cs")
+                handler = CreateHandler()
+
+                Using LinkHandlerHelpers.InitializeRepository(dir.FullPath)
+                End Using
+
+                Assert.Equal(
+                    "https://local-bitbucket:7990/context/projects/bb/repos/my-code/browse/lib/server/main.cs?at=refs%2Fheads%2Fmaster",
+                    handler.MakeUrl(info, fileName, Nothing)
+                )
+            End Using
+        End Sub
+
+
+        <Theory()>
+        <MemberData(NameOf(GetRemotes), MemberType:=GetType(BitbucketServerHandlerTests))>
+        Public Sub CreatesCorrectLinkFromHttpRemote(remote As String)
+            Using dir As New TempDirectory
+                Dim handler As BitbucketServerHandler
+                Dim info As GitInfo
+                Dim fileName As String
+
+
+                ' Swap the `https` URL for an `http` URL.
+                If remote.StartsWith("https://", StringComparison.Ordinal) Then
+                    remote = "http://" & remote.Substring("https://".Length)
+                End If
+
+                info = New GitInfo(dir.FullPath, remote)
+                fileName = Path.Combine(dir.FullPath, "lib\server\main.cs")
+                handler = CreateHandler({New ServerUrl("http://local-bitbucket:7990/context", "git@local-bitbucket:7999")})
+
+                Using LinkHandlerHelpers.InitializeRepository(dir.FullPath)
+                End Using
+
+                Assert.Equal(
+                    "http://local-bitbucket:7990/context/projects/bb/repos/my-code/browse/lib/server/main.cs?at=refs%2Fheads%2Fmaster",
+                    handler.MakeUrl(info, fileName, Nothing)
+                )
+            End Using
+        End Sub
+
+
+        <Fact()>
+        Public Sub CreatesCorrectLinkWhenServerUrlEndsWithSlash()
+            Using dir As New TempDirectory
+                Dim handler As BitbucketServerHandler
+                Dim info As GitInfo
+                Dim fileName As String
+
+
+                info = New GitInfo(dir.FullPath, GetHttpRemoteUrl())
+                fileName = Path.Combine(dir.FullPath, "lib\server\main.cs")
+                handler = CreateHandler({New ServerUrl("https://local-bitbucket:7990/context/", "git@local-bitbucket:7999")})
+
+                Using LinkHandlerHelpers.InitializeRepository(dir.FullPath)
+                End Using
+
+                Assert.Equal(
+                    "https://local-bitbucket:7990/context/projects/bb/repos/my-code/browse/lib/server/main.cs?at=refs%2Fheads%2Fmaster",
+                    handler.MakeUrl(info, fileName, Nothing)
+                )
+            End Using
+        End Sub
+
+
+        <Fact()>
+        Public Sub CreatesCorrectLinkWithSingleLineSelection()
+            Using dir As New TempDirectory
+                Dim handler As BitbucketServerHandler
+                Dim info As GitInfo
+                Dim fileName As String
+
+
+                info = New GitInfo(dir.FullPath, GetGitRemoteUrl())
+                fileName = Path.Combine(dir.FullPath, "lib\server\main.cs")
+                handler = CreateHandler()
+
+                Using LinkHandlerHelpers.InitializeRepository(dir.FullPath)
+                End Using
+
+                Assert.Equal(
+                    "https://local-bitbucket:7990/context/projects/bb/repos/my-code/browse/lib/server/main.cs?at=refs%2Fheads%2Fmaster#2",
+                    handler.MakeUrl(info, fileName, New LineSelection(2, 2))
+                )
+            End Using
+        End Sub
+
+
+        <Fact()>
+        Public Sub CreatesCorrectLinkWithMultiLineSelection()
+            Using dir As New TempDirectory
+                Dim handler As BitbucketServerHandler
+                Dim info As GitInfo
+                Dim fileName As String
+
+
+                info = New GitInfo(dir.FullPath, GetGitRemoteUrl())
+                fileName = Path.Combine(dir.FullPath, "lib\server\main.cs")
+                handler = CreateHandler()
+
+                Using LinkHandlerHelpers.InitializeRepository(dir.FullPath)
+                End Using
+
+                Assert.Equal(
+                    "https://local-bitbucket:7990/context/projects/bb/repos/my-code/browse/lib/server/main.cs?at=refs%2Fheads%2Fmaster#10-23",
+                    handler.MakeUrl(info, fileName, New LineSelection(10, 23))
+                )
+            End Using
+        End Sub
+
+
+        <Fact()>
+        Public Sub UsesCurrentBranch()
+            Using dir As New TempDirectory
+                Dim handler As BitbucketServerHandler
+                Dim info As GitInfo
+                Dim fileName As String
+
+
+                info = New GitInfo(dir.FullPath, GetGitRemoteUrl())
+                fileName = Path.Combine(dir.FullPath, "lib\server\main.cs")
+                handler = CreateHandler()
+
+                Using repo = LinkHandlerHelpers.InitializeRepository(dir.FullPath)
+                    LibGit2Sharp.Commands.Checkout(repo, repo.CreateBranch("feature/thing"))
+                End Using
+
+                Assert.Equal(
+                    "https://local-bitbucket:7990/context/projects/bb/repos/my-code/browse/lib/server/main.cs?at=refs%2Fheads%2Ffeature%2Fthing",
+                    handler.MakeUrl(info, fileName, Nothing)
+                )
+            End Using
+        End Sub
+
+    End Class
+
+
+    Public Shared Iterator Function GetRemotes() As IEnumerable(Of Object())
+        Yield {GetHttpRemoteUrl()}
+        Yield {GetHttpRemoteUrl().Replace("https://", "https://username@")}
+        Yield {GetGitRemoteUrl()}
+        Yield {$"ssh://{GetGitRemoteUrl()}"}
+    End Function
+
+
+    Private Shared Function GetHttpRemoteUrl() As String
+        Return "https://local-bitbucket:7990/context/scm/bb/my-code.git"
+    End Function
+
+
+    Private Shared Function GetGitRemoteUrl() As String
+        Return "git@local-bitbucket:7999/bb/my-code.git"
+    End Function
+
+
+    Private Shared Function CreateHandler(Optional servers() As ServerUrl = Nothing) As BitbucketServerHandler
+        Dim options As Mock(Of IOptions)
+
+
+        If servers Is Nothing Then
+            servers = {New ServerUrl("https://local-bitbucket:7990/context", "git@local-bitbucket:7999")}
+        End If
+
+        options = New Mock(Of IOptions)
+        options.SetupGet(Function(x) x.BitbucketServerUrls).Returns(servers)
+
+        Return New BitbucketServerHandler(options.Object)
+    End Function
+
+End Class
