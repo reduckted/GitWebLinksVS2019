89916d812fc8b2bcabdfb1e923e44385dfa250ad
reduckted <reduckted@outlook.com>
2017-07-16 14:09:23 +1000

    Added build scripts.

---
+++ build/scripts/build.ps1
@@ -0,0 +1,40 @@
+Set-StrictMode -Version "Latest"
+$ErrorActionPreference = "Stop"
+
+$root = Split-Path -Path $PSCommandPath -Parent | Join-Path -ChildPath "..\.." | Resolve-Path
+
+$msbuild = ""
+
+foreach ($edition in @("Community", "Professional", "Enterprise")) {
+    $exe = Join-Path -Path ${env:ProgramFiles(x86)} -ChildPath "Microsoft Visual Studio\2017\$edition\MSBuild\15.0\Bin\msbuild.exe"
+
+    if (Test-Path $exe) {
+        $msbuild = $exe
+        break;
+    }
+}
+
+if (-not $msbuild) {
+    throw "Could not find MSBuild."
+}
+
+$solution = Join-Path -Path $root -ChildPath "GitWebLinks.sln"
+
+& $msbuild $solution /t:Rebuild /p:Configuration=Release /v:m
+
+if ($LASTEXITCODE -ne 0) {
+    throw "Build failed."
+}
+
+$xunit = Join-Path -Path $root -ChildPath "packages\xunit.runner.console.2.2.0\tools\xunit.console.exe"
+$assembly = Join-Path -Path $root -ChildPath "tests\GitWebLinks.Tests\bin\Release\GitWebLinks.Tests.dll"
+
+& $xunit $assembly
+
+if ($LASTEXITCODE -ne 0) {
+    throw "Tests failed."
+}
+
+$extension = Join-Path -Path $root -ChildPath "source\GitWebLinks\bin\Release\GitWebLinks.vsix"
+
+explorer.exe "/select,`"$extension`""
---
+++ build/scripts/version.ps1
@@ -0,0 +1,70 @@
+[CmdLetBinding()]
+param (
+    [switch] $Major,
+    [switch] $Minor,
+    [switch] $Patch
+)
+
+Set-StrictMode -Version "Latest"
+$ErrorActionPreference = "Stop"
+
+$root = Split-Path -Path $PSCommandPath -Parent | Join-Path -ChildPath "..\.." | Resolve-Path
+
+# Find the current version.
+$assemblyInfo = $root | Join-Path -ChildPath "source\GitWebLinks\My Project\AssemblyInfo.vb"
+$content = Get-Content -Path $assemblyInfo -Raw
+
+if (-not ($content -match "\<Assembly\: AssemblyVersion\(`"([\d\.]+)`"\)\>")) {
+    throw "Could not determine the current version."
+}
+
+# Determine the new version.
+$previous = $matches[1]
+$version = New-Object System.Version -ArgumentList $previous
+
+if ($Major) {
+    $version = New-Object System.Version -ArgumentList ($version.Major + 1), 0, 0, 0
+
+} elseif ($Minor) {
+    $version = New-Object System.Version -ArgumentList $version.Major, ($version.Minor + 1), 0, 0
+
+} elseif ($Patch) {
+    $version = New-Object System.Version -ArgumentList $version.Major, $version.Minor, ($version.Build + 1), 0
+}
+
+$assemblyVersion = "$($version.Major).$($version.Minor).$($version.Build).$($version.Revision)"
+$vsixVersion = "$($version.Major).$($version.Minor).$($version.Build)"
+
+# Update the assembly info files.
+Get-ChildItem -Path $root -Recurse -Filter "AssemblyInfo.vb" | ForEach-Object {
+    $s = Get-Content -Path $_.FullName -Raw -Encoding UTF8
+    $s = $s -creplace "Version\(`"([\d\.]+)`"\)", "Version(`"$assemblyVersion`")"
+    [System.IO.File]::WriteAllText($_.FullName, $s, [System.Text.Encoding]::UTF8)
+}
+
+# Update the VSIX manifest files.
+Get-ChildItem -Path $root -Recurse -Filter "*.vsixmanifest" | ForEach-Object {
+    $doc = [xml](Get-Content -Path $_.FullName)
+
+    if ($doc) {
+        $ns = New-Object System.Xml.XmlNamespaceManager -ArgumentList $doc.NameTable
+        $ns.AddNamespace("x", $doc.DocumentElement.NamespaceURI)
+
+        $identity = $doc.SelectSingleNode("x:PackageManifest/x:Metadata/x:Identity", $ns)
+        $identity.SetAttribute("Version", $vsixVersion)
+
+        $writer = [System.IO.StreamWriter]::new($_.FullName, $false, [System.Text.Encoding]::UTF8)
+
+        try {
+            $doc.Save($writer)
+            $writer.WriteLine("")
+
+        } finally {
+            $writer.Close()
+        }
+    }
+}
+
+# Stage the changes and commit.
+git add .
+git commit -m "v$vsixVersion"
